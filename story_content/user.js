a38cd9b860d478adbb8c72d34034d6b81c44a94a
Edwin Merced <edwinmerced@gmail.com>
2019-10-15 23:02:28 -0400

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,132 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6DTdGviLYfE":
+        Script1();
+        break;
+      case "6L3J6QhjHpJ":
+        Script2();
+        break;
+      case "637c7nY0PM2":
+        Script3();
+        break;
+      case "6GXgOSxL4WQ":
+        Script4();
+        break;
+      case "6R9sswG80e4":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (document.location.href.indexOf('html5') < 0) {
+GetPlayer().printSlide()
+} else {
+if(!window.hasPrintStyle){
+window.hasPrintStyle = true;
+var css = "@media print {div.controls.grid-row.inflexible,div.area-secondary.cs-left.inflexible,header.header-primary.centered-title.extended-height,div.presentation-wrapper:after {display:none !important; visibility:hidden !important;}}";
+head = document.head || document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = css;
+} else {
+style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+}
+window.print();
+}
+}
+
+function Script2()
+{
+  if (document.location.href.indexOf('html5') < 0) {
+GetPlayer().printSlide()
+} else {
+if(!window.hasPrintStyle){
+window.hasPrintStyle = true;
+var css = "@media print {div.controls.grid-row.inflexible,div.area-secondary.cs-left.inflexible,header.header-primary.centered-title.extended-height,div.presentation-wrapper:after {display:none !important; visibility:hidden !important;}}";
+head = document.head || document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = css;
+} else {
+style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+}
+window.print();
+}
+}
+
+function Script3()
+{
+  if (document.location.href.indexOf('html5') < 0) {
+GetPlayer().printSlide()
+} else {
+if(!window.hasPrintStyle){
+window.hasPrintStyle = true;
+var css = "@media print {div.controls.grid-row.inflexible,div.area-secondary.cs-left.inflexible,header.header-primary.centered-title.extended-height,div.presentation-wrapper:after {display:none !important; visibility:hidden !important;}}";
+head = document.head || document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = css;
+} else {
+style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+}
+window.print();
+}
+}
+
+function Script4()
+{
+  if (document.location.href.indexOf('html5') < 0) {
+GetPlayer().printSlide()
+} else {
+if(!window.hasPrintStyle){
+window.hasPrintStyle = true;
+var css = "@media print {div.controls.grid-row.inflexible,div.area-secondary.cs-left.inflexible,header.header-primary.centered-title.extended-height,div.presentation-wrapper:after {display:none !important; visibility:hidden !important;}}";
+head = document.head || document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = css;
+} else {
+style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+}
+window.print();
+}
+}
+
+function Script5()
+{
+  if (document.location.href.indexOf('html5') < 0) {
+GetPlayer().printSlide()
+} else {
+if(!window.hasPrintStyle){
+window.hasPrintStyle = true;
+var css = "@media print {div.controls.grid-row.inflexible,div.area-secondary.cs-left.inflexible,header.header-primary.centered-title.extended-height,div.presentation-wrapper:after {display:none !important; visibility:hidden !important;}}";
+head = document.head || document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = css;
+} else {
+style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+}
+window.print();
+}
+}
+
